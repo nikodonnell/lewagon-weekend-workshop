144ab9ac99745dd9f90695a100cbb5051b63cff6
Nik O'Donnell <nikodonnell@gmail.com>
2017-09-17 16:14:46 +0200

    Update awesomo.rb

--- day-1-ruby-basics/04-Awesomo-4000/lib/awesomo.rb
+++ day-1-ruby-basics/04-Awesomo-4000/lib/awesomo.rb
@@ -23,24 +23,32 @@ PUNCHLINE = ['a doorman who always highfives children of divorce',
 
 def plot_generator
   # TODO: call the submethods correctly
+
+  "#{pick_actor} #{pick_setting} #{pick_closed_one} #{pick_punchline}"
+
 end
 
+#T0D0: create 4 methods that each return one random element from their respective arrays
 
 def pick_actor
   # TODO: Your code here
+  ACTOR.sample
 end
 
 def pick_setting
   # TODO: Your code here
+  SETTING.sample
 end
 
 def pick_closed_one
   # TODO: Your code here
+  CLOSED.sample
 end
 
 def pick_punchline
   # TODO: Your code here
+  PUNCHLINE.sample
 end
-#T0D0: create 4 methods that each return one random element from their respective arrays
+
 
 
