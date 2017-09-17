d88fd67e72697d608b9a64bc063644af3159a904
Eric van der Helm <evand@biosustain.dtu.dk>
2017-09-17 12:50:14 +0200

    Make spec file test the correct function
    Instead of testing wagon_sort should test workshop_sort

--- day-2-flow-arrays-iteration/03-Sorting-the-workshop/spec/wagon_sort_spec.rb
+++ day-2-flow-arrays-iteration/03-Sorting-the-workshop/spec/wagon_sort_spec.rb
@@ -2,14 +2,14 @@ require "workshop_sort"
 
 describe "#workshop_sort" do
   it "should return an empty array if given an empty array" do
-    expect(wagon_sort([])).to eq []
+    expect(workshop_sort([])).to eq []
   end
 
   it "should sort all-caps students" do
-    expect(wagon_sort(["BOB", "ALICE", "CHARLIE"])).to eq(["ALICE", "BOB", "CHARLIE"])
+    expect(workshop_sort(["BOB", "ALICE", "CHARLIE"])).to eq(["ALICE", "BOB", "CHARLIE"])
   end
 
   it "should be case-insensitive" do
-    expect(wagon_sort(["BOB", "alice", "CHARLIE"])).to eq(["alice", "BOB", "CHARLIE"])
+    expect(workshop_sort(["BOB", "alice", "CHARLIE"])).to eq(["alice", "BOB", "CHARLIE"])
   end
 end
