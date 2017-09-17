3fe7e23e50402790e1e61bf40081d16033ad4f83
Nik O'Donnell <nikodonnell@gmail.com>
2017-09-17 16:12:48 +0200

    Update interface.rb

--- day-1-ruby-basics/02-Whats-Your-Name/lib/interface.rb
+++ day-1-ruby-basics/02-Whats-Your-Name/lib/interface.rb
@@ -6,3 +6,16 @@ require_relative "compute_name"
 # TODO: Print a greeting to the user with the full name concatenated,
 #       something like "Hello, Boris Paillard"
 
+puts "What's your first name?"
+
+first_name = gets.chomp
+
+puts "What's your middle name?"
+
+middle_name = gets.chomp
+
+puts "whats your last name?"
+
+last_name = gets.chomp
+
+puts "Hello #{compute_name(first_name, middle_name, last_name)}"
