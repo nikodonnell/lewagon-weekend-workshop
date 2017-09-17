8f4f6b0dc244cbd6c4eab712fc4808db5fc3aabe
Nik O'Donnell <nikodonnell@gmail.com>
2017-09-17 16:17:42 +0200

    Update interface.rb

--- day-1-ruby-basics/05-Guess-Number/lib/interface.rb
+++ day-1-ruby-basics/05-Guess-Number/lib/interface.rb
@@ -1,3 +1,33 @@
 require_relative 'guess_number'
 
+
 # TODO: create the flow of your game, with instructions and user inputs
+
+
+puts "Hello there, what's your name!"
+
+name = gets.chomp
+
+puts "Welcome #{name} Time to guess a number between 1 and 20!"
+
+user_number = gets.chomp.to_i
+
+right_answer = random_number()
+
+until right_answer == user_number
+
+  if user_number > right_answer
+    puts "Too High"
+
+  elsif user_number < right_answer
+    puts "Too low"
+
+end
+    puts "Guess again"
+
+    user_number = gets.chomp.to_i
+
+end
+
+puts "Well done, you guessed right!!!"
+
