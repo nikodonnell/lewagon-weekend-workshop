3e9ca2b4e3f372b37809fb876f014cf3d6a2cfb1
Nik O'Donnell <nikodonnell@gmail.com>
2017-09-17 16:17:16 +0200

    Update guess_number.rb

--- day-1-ruby-basics/05-Guess-Number/lib/guess_number.rb
+++ day-1-ruby-basics/05-Guess-Number/lib/guess_number.rb
@@ -1,11 +1,14 @@
 def random_number
   # Generate random number from 1 to 20
+  rand(1..20)
 end
 
 def right_number?(your_guess, right_answer)
   # Check if the guess if right
+  your_guess == right_answer
 end
 
 def greater_number?(your_guess, right_answer)
   # Check if the guess is higher than answer
+  your_guess > right_answer
 end
