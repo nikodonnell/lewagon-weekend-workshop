57f4911a2e15156785c63b69bc58b2aca6e19ab5
Nik O'Donnell <nikodonnell@gmail.com>
2017-09-17 16:20:09 +0200

    Update interface.rb

--- day-2-flow-arrays-iteration/03-Sorting-the-workshop/lib/interface.rb
+++ day-2-flow-arrays-iteration/03-Sorting-the-workshop/lib/interface.rb
@@ -5,7 +5,16 @@ require_relative "workshop_sort"
 #       - `gets`:  http://www.ruby-doc.org/core-2.2.0/Kernel.html#method-i-gets
 #       - `chomp`: http://www.ruby-doc.org/core-2.2.0/String.html#method-i-chomp
 
-
+puts "Type a students name in the workshop"
+student = gets.chomp
+array.push(student)
 
 # TODO: Then call `workshop_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
+    puts "Add another student to the workshop, or press enter to Enter to finish" if student != ""
+
+    student = gets.chomp
+    array.push(student)
+  else
+    puts "Congrats! You've added the following students to your workshop #{workshop_sort()}
+
