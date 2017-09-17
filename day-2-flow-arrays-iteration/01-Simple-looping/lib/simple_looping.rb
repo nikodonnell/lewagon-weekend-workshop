cfe076dc370e20c12ff1d9ed5ba25fb81291754a
Nik O'Donnell <nikodonnell@gmail.com>
2017-09-17 16:18:50 +0200

    Update simple_looping.rb

--- day-2-flow-arrays-iteration/01-Simple-looping/lib/simple_looping.rb
+++ day-2-flow-arrays-iteration/01-Simple-looping/lib/simple_looping.rb
@@ -1,7 +1,19 @@
 def sum_with_while(min, max)
   # CONSTRAINT: you should use a while..end structure
-end
 
-def sum_with_for(min, max)
-  # CONSTRAINT: you should use a for..end structure
+#while min <= max
+#    (min..max).reduce(:+)
+#  end
+#end
+
+return -1 if min > max
+if min > max
+  return -1
+
+sum = 0
+while min <= max
+
+  sum = sum + min
+  min = min +1
 end
+return sum
