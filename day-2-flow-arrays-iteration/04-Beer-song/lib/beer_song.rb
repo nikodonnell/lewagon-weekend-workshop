b584536e3d92386c1ee7798626fdfca5471b3256
Nik O'Donnell <nikodonnell@gmail.com>
2017-09-17 16:20:58 +0200

    Update beer_song.rb

--- day-2-flow-arrays-iteration/04-Beer-song/lib/beer_song.rb
+++ day-2-flow-arrays-iteration/04-Beer-song/lib/beer_song.rb
@@ -1,3 +1,13 @@
 def beer_song(beer_start_count)
   #TODO: sing the song
+
+  puts "Time to sing bottle song, how many bottles of beer are on the wall?"
+  bottles = gets.chomp
+
+  while bottles > 0
+    puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer"
+    bottles = bottles - 1
+    puts "you take one down pass it around #{bottles}"
+  end
+
 end
