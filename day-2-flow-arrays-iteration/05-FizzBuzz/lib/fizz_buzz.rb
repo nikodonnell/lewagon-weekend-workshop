6827d8f02ef74ce382af177f876b05138b723e28
Nik O'Donnell <nikodonnell@gmail.com>
2017-09-17 16:21:21 +0200

    Update fizz_buzz.rb

--- day-2-flow-arrays-iteration/05-FizzBuzz/lib/fizz_buzz.rb
+++ day-2-flow-arrays-iteration/05-FizzBuzz/lib/fizz_buzz.rb
@@ -1,3 +1,18 @@
 def fizz_buzz(number)
   # TODO: return an array of integers, 'Fizz', 'Buzz' or 'FizzBuzz'
+  array = Array.new
+  (1..number).each do |i|
+    if i % 3 == 0 && i % 5 ==0
+      array.push("FizzBuzz")
+    elsif i % 3 == 0
+      array.push("Fizz")
+    elsif i % 5 == 0
+      array.push("Buzz")
+    else
+      array.push(i)
+    end
+  end
+  puts array
 end
+
+fizz_buzz(20)
